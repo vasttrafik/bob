a856fdbcfce0079c3e9a1d614c34aa7a5b6c6893
Viktor Molin <molin.viktor@server2496.vt.net>
2025-05-13 13:46:22 +0200

    remove 2017-1 endpoints

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -4,15 +4,10 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     urls: [{url: "authentication-v1-swagger.json", name: "Authentication v1"},
-           {url: "device-v1-swagger.json", name: "Device v1"},
            {url: "device-v2-swagger.json", name: "Device v2"},
-           {url: "inspection-v1-swagger.json", name: "Inspection v1"},
            {url: "inspection-v2-swagger.json", name: "Inspection v2"},
-           {url: "product-v1-swagger.json", name: "Product v1"},
            {url: "product-v3-swagger.json", name: "Product v3"},
-           {url: "ticket-v1-swagger.json", name: "Ticket v1"},
            {url: "ticket-v3-swagger.json", name: "Ticket v3"},
-           {url: "validation-v1-swagger.json", name: "Validation v1"},
            {url: "validation-v3-swagger.json", name: "Validation v3"}],
     dom_id: '#swagger-ui',
     deepLinking: true,
